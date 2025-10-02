5bd1771808965a91902803b201d05b22d28ed3ce
Pavel Koyushev <pkoyushev@gmail.com>
2025-10-02 23:18:26 +0300

    target

--- Package.swift
+++ Package.swift
@@ -11,13 +11,13 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "OpenCV-package",
-            targets: ["OpenCV-package"]
-        ),
+            name: "OpenCV",
+            targets: ["OpenCV"]
+        )
     ],
     targets: [
         .binaryTarget(
-            name: "OpenCV-package",
+            name: "OpenCV",
             url: "https://github.com/PavelKoyushev/OpenCV-package/releases/download/1.0.0/opencv2.xcframework.zip",
             checksum: "57134608721f892e8a529475aefd060b616ab2e343e6cbe2435cdf58773751d7"
         )
